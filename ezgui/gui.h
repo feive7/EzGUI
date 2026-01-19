c4a51dc03b72c68e69672b154adcb68d9703d96f
rolan <rolan@DESKTOP-MOG1UQ9>
2026-01-19 10:59:00 -0800

    added labels

--- ezgui/gui.h
+++ ezgui/gui.h
@@ -1,18 +1,42 @@
 #pragma once
+#include <vector>
 #include "raylib.h"
+
+struct Label {
+	int x;
+	int y;
+	int font_size;
+	const char* text;
+};
 
 class GUI {
 	void draw() {
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-		DrawText("Hello Raylib!", 5, 5, 20, BLACK);
+		for (const Label& label : labels) {
+			DrawText(label.text, label.x, label.y, label.font_size, BLACK);
+		}
 		EndDrawing();
 	}
 public:
+	// Window properties
 	const int width;
 	const int height;
 	const char* title;
+
+	// Window contents
+	std::vector<Label> labels;
+
+	// GUI initializer
 	GUI(int width, int height, const char* title) : width(width), height(height), title(title) {}
+
+	// GUI constructors
+	void addLabel(int x, int y, int font_size, const char* text) {
+		Label new_label = { x,y,font_size,text };
+		labels.push_back(new_label);
+	}
+
+	// Main GUI loop
 	void show() {
 		// Load window assets
 		Image favicon = LoadImage("../../../favicon.png");
