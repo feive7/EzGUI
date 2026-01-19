60df18e0c361c5667d9a4b3ffba8dace79179289
rolan <rolan@DESKTOP-MOG1UQ9>
2026-01-19 12:47:08 -0800

    added click detectors

--- ezgui/gui.h
+++ ezgui/gui.h
@@ -2,19 +2,38 @@
 #include <vector>
 #include "raylib.h"
 
+#include "area.h"
+
 struct Label {
 	int x;
 	int y;
 	int font_size;
 	const char* text;
 };
+struct ClickDetector {
+	Area area;
+	const char* command;
+};
 
 class GUI {
+	void handle() {
+		Vector2 mpos = GetMousePosition();
+		bool click = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+		for (const ClickDetector& cd : clickdetectors) {
+			bool inside_cd = PointInArea(mpos, cd.area);
+			if (inside_cd && click) {
+				system(cd.command);
+			}
+		}
+	}
 	void draw() {
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		for (const Label& label : labels) {
 			DrawText(label.text, label.x, label.y, label.font_size, BLACK);
+		}
+		for (const ClickDetector& cd : clickdetectors) {
+			DrawArea(cd.area);
 		}
 		EndDrawing();
 	}
@@ -26,6 +45,7 @@ public:
 
 	// Window contents
 	std::vector<Label> labels;
+	std::vector<ClickDetector> clickdetectors;
 
 	// GUI initializer
 	GUI(int width, int height, const char* title) : width(width), height(height), title(title) {}
@@ -35,19 +55,32 @@ public:
 		Label new_label = { x,y,font_size,text };
 		labels.push_back(new_label);
 	}
+	void addButton(int x, int y, int font_size, const char* text, const char* command, int padding = 2) {
+		Label new_label = { x,y,font_size,text };
+		labels.push_back(new_label);
+
+		int text_measure = TextLength(text) * font_size * 0.575;
+
+		Area click_area = {
+			x - padding,y - padding, // Minimum
+			x + text_measure + padding, y + font_size + padding
+		};
+		ClickDetector new_cd = { click_area,command };
+		clickdetectors.push_back(new_cd);
+	}
 
 	// Main GUI loop
 	void show() {
 		// Load window assets
 		Image favicon = LoadImage("../../../favicon.png");
-
 		// Create window
 		InitWindow(width, height, title);
 		SetWindowIcon(favicon);
 
 		// Draw the window while open
 		while (!WindowShouldClose()) {
-			draw();
+			handle(); // Handle clicks
+			draw();   // Draw GUI
 		}
 
 		// Close window
