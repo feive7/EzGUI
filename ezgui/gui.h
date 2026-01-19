da76721588459822dc7ce775aa0298a11f2ff6cf
rolan <rolan@DESKTOP-MOG1UQ9>
2026-01-19 14:28:32 -0800

    added image labels

--- ezgui/gui.h
+++ ezgui/gui.h
@@ -5,19 +5,31 @@
 struct TextLabel {
 	Vector2 pos;      // Label position
 	int font_size;    // Label font size
+	Color font_color; // Label text color
 	const char* text; // Label text
 };
 struct ClickDetector {
 	Rectangle rect;      // Position and size
 	const char* command; // Command to run on click
 };
-
 struct ColorRect {
 	Rectangle rect;
-	Color color;
+	Color bg_color;
+	Color border_color;
+};
+struct ImageRect {
+	Rectangle rect;
+	Image image;
+
+	Texture _texture;
 };
 
 class GUI {
+	void init() {
+		for (ImageRect& ir : imagerects) {
+			ir._texture = LoadTextureFromImage(ir.image);
+		}
+	}
 	void handle() {
 		Vector2 mpos = GetMousePosition();
 		bool click = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
@@ -29,13 +41,20 @@ class GUI {
 		}
 	}
 	void draw() {
+		// TO-DO:
+		// - Add drawing order
+
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+		for (const ImageRect& ir : imagerects) {
+			DrawTexturePro(ir._texture, { 0,0,(float)ir.image.width,(float)ir.image.height }, ir.rect, { 0 }, 0, WHITE);
+		}
 		for (const ColorRect& cr : colorrects) {
-			DrawRectangleRec(cr.rect,cr.color);
+			DrawRectangleRec(cr.rect, cr.bg_color);
+			DrawRectangleLinesEx(cr.rect,2.0f,cr.border_color);
 		}
 		for (const TextLabel& label : labels) {
-			DrawTextEx(GetFontDefault(), label.text, label.pos, label.font_size, 2, BLACK);
+			DrawTextEx(GetFontDefault(), label.text, label.pos, label.font_size, 2, label.font_color);
 		}
 		EndDrawing();
 	}
@@ -46,6 +65,7 @@ public:
 	const char* title;
 
 	// Window contents
+	std::vector<ImageRect> imagerects;
 	std::vector<ColorRect> colorrects;
 	std::vector<TextLabel> labels;
 	std::vector<ClickDetector> clickdetectors;
@@ -54,12 +74,19 @@ public:
 	GUI(int width, int height, const char* title) : width(width), height(height), title(title) {}
 
 	// GUI constructors
-	void addTextLabel(int x, int y, int font_size, const char* text) {
-		TextLabel new_label = {x,y,font_size,text};
+	void addTextLabel(int x, int y, int font_size, Color font_color, const char* text) {
+		TextLabel new_label = { x,y,font_size,font_color,text };
 		labels.push_back(new_label);
 	}
-	void addTextButton(int x, int y, int font_size, const char* text, const char* command, int padding = 2) {
-		TextLabel new_label = { x,y,font_size,text };
+	void addImageLabel(int x, int y, int width, int height, const char* filename) {
+		Rectangle label_rect = { x,y,width,height };
+		Image image = LoadImage(filename);
+		ImageResize(&image, width, height);
+		ImageRect new_image_label = { label_rect,image };
+		imagerects.push_back(new_image_label);
+	}
+	void addTextButton(int x, int y, int font_size, Color font_color, const char* text, Color bg_color, Color border_color, const char* command, int padding = 2) {
+		TextLabel new_label = { x,y,font_size,font_color,text };
 		labels.push_back(new_label);
 
 		int text_measure = TextLength(text) * font_size * 0.575;
@@ -72,7 +99,7 @@ public:
 		ClickDetector new_cd = { click_area, command };
 		clickdetectors.push_back(new_cd);
 
-		ColorRect new_colorrect = { click_area, GRAY };
+		ColorRect new_colorrect = { click_area, bg_color, border_color };
 		colorrects.push_back(new_colorrect);
 	}
 
@@ -83,6 +110,9 @@ public:
 		// Create window
 		InitWindow(width, height, title);
 		SetWindowIcon(favicon);
+
+		// Init GUI assets
+		init();
 
 		// Draw the window while open
 		while (!WindowShouldClose()) {
--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 int main() {
 	// Create GUI
 	GUI main_gui(500,500,"EzGUI");
-	main_gui.addTextLabel(40, 64, 20, "Hello EzGUI!");
-	main_gui.addTextButton(200, 200, 40, "Button", "echo Hello World");
+	main_gui.addImageLabel(0, 0, 500, 500, "../../../test_image.png");
+	main_gui.addTextLabel(5, 5, 20, WHITE, "Hello EzGUI!");
+	main_gui.addTextButton(200, 200, 40, WHITE, "Hello!", { 200,200,255,255 }, { 100,100,255,255 }, "echo Hello World");
 
 	main_gui.show();
 
