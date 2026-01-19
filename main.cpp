308cb9a737763a0bbc4ead18d442c03e8f8f9f8f
rolan <rolan@DESKTOP-MOG1UQ9>
2026-01-19 10:10:31 -0800

    basic raylib setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "raylib.h"
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	// Define window dimensions
+	const int window_width = 800;
+	const int window_height = 600;
+
+	// Create window
+	InitWindow(window_width, window_height, "EzGUI Demo");
+
+	// Main loop
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("Hello Raylib!", 5, 5, 20, BLACK);
+		EndDrawing();
+	}
+
+	// Close window
+	CloseWindow();
+
 	return 0;
 }
